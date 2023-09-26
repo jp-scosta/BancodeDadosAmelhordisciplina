9bd85b1f14db5d05247ebb196545fba0b7455d25
jp-scosta <102810085+jp-scosta@users.noreply.github.com>
2023-09-26 20:46:18 -0300

    Update listaExercicios02.sql
    6. Extração de Títulos por Categoria
    
    Crie uma stored procedure chamada sp_TitulosPorCategoria que liste apenas os títulos dos livros de uma categoria específica.

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -71,3 +71,16 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+-- Exercício 6
+
+DELIMITER //
+CREATE PROCEDURE sp_TitulosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END;
+//
+DELIMITER ;
