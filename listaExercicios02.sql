e4daf62fe7a531234b5310fe5eef87fefbc976d1
jp-scosta <102810085+jp-scosta@users.noreply.github.com>
2023-09-26 20:39:40 -0300

    Update listaExercicios02.sql
    2. Livros por Categoria
    
    Desenvolva uma stored procedure chamada sp_LivrosPorCategoria que aceite o nome de uma categoria como parâmetro e retorne todos os livros que pertencem a essa categoria. Não se esqueça de testar a procedure com diferentes categorias.

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -9,3 +9,19 @@ END;
 DELIMITER ;
 
 CALL sp_ListarAutores();
+
+-- Exercício 2
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END;
+//
+DELIMITER ;
+
+CALL sp_LivrosPorCategoria('Ciência');
+CALL sp_LivrosPorCategoria('Ficção Científica');
