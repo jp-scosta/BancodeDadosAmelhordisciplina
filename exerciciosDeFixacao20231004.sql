5fb8fd3a6cf80e1570f765a4e9d56692236695b2
jp-scosta <102810085+jp-scosta@users.noreply.github.com>
2023-10-16 19:25:40 -0300

    Update exerciciosDeFixacao20231004.sql
    Crie uma função chamada listar_livros_por_autor que aceite o primeiro e o último nome de um autor como parâmetros. A função deve retornar uma lista de títulos de livros associados a esse autor. Utilize um cursor para buscar na tabela Livro_Autor e depois na tabela Livro.

--- exerciciosDeFixacao20231004.sql
+++ exerciciosDeFixacao20231004.sql
@@ -31,3 +31,39 @@ END;
 //
 DELIMITER ;
 
+-- 2. Função para Listar Livros de um Autor Específico
+
+DELIMITER //
+CREATE FUNCTION listar_livros_por_autor(primeiro_nome VARCHAR(255), ultimo_nome VARCHAR(255)) RETURNS TEXT
+BEGIN
+    DECLARE lista_de_livros TEXT DEFAULT '';
+    DECLARE done INT DEFAULT 0;
+    DECLARE livro_id INT;
+    
+    DECLARE cur CURSOR FOR
+    SELECT LA.id_livro
+    FROM Livro_Autor LA
+    JOIN Autor A ON LA.id_autor = A.id
+    WHERE A.primeiro_nome = primeiro_nome AND A.ultimo_nome = ultimo_nome;
+    
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+    
+    OPEN cur;
+    
+    read_loop: LOOP
+        FETCH cur INTO livro_id;
+        
+        IF done = 1 THEN
+            LEAVE read_loop;
+        END IF;
+        
+        SELECT titulo INTO lista_de_livros FROM Livro WHERE id = livro_id;
+        SET lista_de_livros = CONCAT(lista_de_livros, ', ', titulo);
+    END LOOP;
+    
+    CLOSE cur;
+    
+    RETURN SUBSTRING(lista_de_livros, 3); -- Remove a vírgula e espaço inicial
+END;
+//
+DELIMITER ;
