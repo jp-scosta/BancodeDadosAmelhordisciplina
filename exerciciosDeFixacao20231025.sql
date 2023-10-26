d8ce75f244b8bc8f64c9979f7cf16b730c83851e
jp-scosta <102810085+jp-scosta@users.noreply.github.com>
2023-10-25 21:45:18 -0300

    Update exerciciosDeFixacao20231025.sql
    Após atualizar o nome de um cliente na tabela Clientes, insira uma mensagem na tabela Auditoria mostrando o nome antigo e o novo nome.

--- exerciciosDeFixacao20231025.sql
+++ exerciciosDeFixacao20231025.sql
@@ -14,3 +14,13 @@ BEGIN
     INSERT INTO Auditoria (mensagem) VALUES ('Tentativa de exclusão de cliente em ' + NOW());
 END;
 
+-- Exercício 3
+
+CREATE TRIGGER AtualizarNomeCliente
+AFTER UPDATE ON Clientes FOR EACH ROW
+BEGIN
+    DECLARE nome_antigo VARCHAR(255);
+    SET nome_antigo = OLD.nome;
+    
+    INSERT INTO Auditoria (mensagem) VALUES ('Nome atualizado de "', nome_antigo, '" para "', NEW.nome, '"'));
+END;
