81d63c83c747a94bb4bacba0f2f9c0df6a7c4b2a
jp-scosta <102810085+jp-scosta@users.noreply.github.com>
2023-10-25 21:40:37 -0300

    Update exerciciosDeFixacao20231025.sql
    Crie um trigger que, após inserir um novo cliente na tabela Clientes, insira uma mensagem na tabela Auditoria informando a data e hora da inserção.

--- exerciciosDeFixacao20231025.sql
+++ exerciciosDeFixacao20231025.sql
@@ -1,1 +1,7 @@
+-- Exercício 1
 
+CREATE TRIGGER clientePosInserir
+AFTER INSERT ON Clientes FOR EACH ROW
+BEGIN
+    INSERT INTO Auditoria (mensagem) VALUES (CONCAT('Novo cliente adicionado em ', NOW()));
+END;
