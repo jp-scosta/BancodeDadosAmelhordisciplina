f28c5fc51739880c9d50852a223c8c3f04643002
jp-scosta <102810085+jp-scosta@users.noreply.github.com>
2023-10-25 21:43:00 -0300

    Update exerciciosDeFixacao20231025.sql
    Antes de excluir um cliente da tabela Clientes, crie um trigger que insira uma mensagem na tabela Auditoria informando sobre a tentativa de exclusão.

--- exerciciosDeFixacao20231025.sql
+++ exerciciosDeFixacao20231025.sql
@@ -5,3 +5,12 @@ AFTER INSERT ON Clientes FOR EACH ROW
 BEGIN
     INSERT INTO Auditoria (mensagem) VALUES (CONCAT('Novo cliente adicionado em ', NOW()));
 END;
+
+-- Exercício 2
+
+CREATE TRIGGER clienteAntesExcluir
+BEFORE DELETE ON Clientes FOR EACH ROW
+BEGIN
+    INSERT INTO Auditoria (mensagem) VALUES ('Tentativa de exclusão de cliente em ' + NOW());
+END;
+
